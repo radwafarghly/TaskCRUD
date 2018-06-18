14341a2c93f3879d5add6e501575b1c22402eb71
radwafarghly <radwa.a.farghly@gmail.com>
2018-06-18 23:56:43 +0200

    crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Hash;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::first()->paginate(5);
+        return view('users.index',compact('users'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+         return view('users.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+         $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|same:confirm-password',
+        ]);
+
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+
+        $user = User::create($input);
+        return redirect()->route('users.index')
+                        ->with('success','User created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+         $user = User::find($id);
+        return view('users.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+         $user = User::find($id);
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email,'.$id,
+            'password' => 'same:confirm-password',
+        ]);
+
+
+        $input = $request->all();
+        if(!empty($input['password'])){ 
+            $input['password'] =bcrypt($input['password']);
+        }else{
+            $input = array_except($input,array('password'));    
+        }
+
+
+        $user = User::find($id);
+        $user->update($input);
+
+        return redirect()->route('users.index')
+                        ->with('success','User updated successfully');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        User::find($id)->delete();
+        return redirect()->route('users.index')
+                        ->with('success','User deleted successfully');
+    }
+}
